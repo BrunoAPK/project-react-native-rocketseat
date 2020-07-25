352b2edf259ccd28618a86989143498fec8b94fe
Brunopro <brunodanielsantos.ti@gmail.com>
2020-07-24 21:05:21 -0300

    ready structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// Elementos no react native não possuem valor semântico (significado)
+// Não possuem estilização própria
+
+// View: div, footer, header, main, aside, section
+// Text: p, span, strong, h1, h2, h3
+
+export default function App() {
+    return <View />;
+}
